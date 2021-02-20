7c30d6db0c77ec801b79adc0cc57ac470c7d0399
HesslerN <nicolehessler2021@u.northwestern.edu>
2021-02-19 20:58:03 -0500

    dictionary-style sentiment analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,6 +5,7 @@ from tweet_reader import *
 from textblob import TextBlob
 import Levenshtein
 import sys
+from emotions import emotions
 
 def host_in_tweet(tweet, hosts):
     '''
@@ -43,7 +44,7 @@ def host_in_tweet(tweet, hosts):
         # has "tinafey"
         host_names_without_space.append(host.replace(" ", ''))
 
-    host_related_words = ["host", "hosted", "hosts"]
+    #host_related_words = ["host", "hosted", "hosts"]
 
     # splitting the tweet's text into individual words and
     # searching for either of the hosts' first or last name, or the whole name
@@ -75,59 +76,61 @@ def host_in_tweet(tweet, hosts):
         poss_match = [word for word in split_tweet_text if are_similar(host_name, word)]
         if poss_match:
             return (0.7, tweet)
-
+    """
     # checking last for matches between host related words (like "hosts", "hosted")
     for host_related_word in host_related_words:
         poss_match = [word for word in split_tweet_text if are_similar(word, host_related_word)]
         if poss_match:
             return (0.7, tweet)
-
+    """
     # this tweet likely isn't associated with either host
     return False
 
 def sa_pipeline(year):
     hosts = get_hosts(year)  # obtain the hosts found for given year
+    print("Loading Tweets...")
     all_tweets = loadTweets(year) # load all tweets for filtering
 
     # mapping the host_in_tweet function (which returns (True, 1.0 or 0.7) or False)
     # on all tweets
-    print("Scanning all tweets for ones that have a strong association with either of the hosts")
-    host_name_filtered_tweets = list(map(lambda tweet: host_in_tweet(tweet, hosts), all_tweets))
+    host_name_filtered_tweets = []
+    limit = 200_000
+    print("Scanning tweets for ones that have a strong association with the host(s)...")
+    for i, tweet in enumerate(all_tweets):
+        r = host_in_tweet(tweet, hosts)
+        if r:
+                host_name_filtered_tweets.append(r)
+        # Progress meter:
+        if i % 1000 == 0 or i + 1 >= limit:
+            progress = (i+1)/limit
+            print("{}{} {}/{} tweets scanned.".format("="*math.floor(progress*50), "-"*(50 - math.floor(progress*50)), i+1, min(limit, len(all_tweets))), end='\r')
+            if i + 1 >= limit: 
+                print("\nFinished scanning tweets.")
+                break
+    
+    #host_name_filtered_tweets = list(map(lambda tweet: host_in_tweet(tweet, hosts), all_tweets))
 
-    polarity_sum = 0
-    num_tweet_with_host_names = 0 # keep track of num of tweets that are assoc. w/ a host name
-    for tweet_tuple in host_name_filtered_tweets:
-        if not tweet_tuple: # meaning that host_in_tweet returned False
-            continue
-        else:
-            num_tweet_with_host_names += 1
+    sentiments = {}
+    for t in host_name_filtered_tweets:
+        for emotion in emotions:
+            if t and emotion in t[1]["text"]:
+                if emotion in sentiments:
+                    sentiments[emotion] += 1
+                else:
+                    sentiments[emotion] = 1
+    
 
-            # tweet_tuple[1] accesses the tweet itself
-            tweet = tweet_tuple[1]
-            tweet_text = cleanTweet(tweet["text"]) # clean the tweet before calc. polarity
-            if not tweet_text: continue
+    hosts = [host.title() for host in hosts]
+    top_emotion = Counter(sentiments).most_common(1)[0][0]
+    result_str = "The sentiment associated with the host(s), "
+    result_str += " and ".join(hosts)
+    result_str += ", seems to be mostly: " + top_emotion + ".\n"
 
-            # polarity is calculated by the TextBlob module,
-            # it is in the range of [-1.0, 1.0]
-            tweet_polarity = (TextBlob(tweet_text)).sentiment.polarity
-            weight = tweet_tuple[0]
-
-            polarity_sum += (tweet_polarity * weight)
-
-    avg_polarity = polarity_sum / num_tweet_with_host_names
-
-    result_str = "The sentiment associated with the hosts: "
-    result_str += ", and ".join(hosts)
-    result_str += ", seems to be..."
-    if -0.3 <= avg_polarity <= 0.3:
-        result_str += "\nFairly neutral! Average Polarity (-1 - negative, +1 - positive): " + str(avg_polarity)
-
-    if -1.0 <= avg_polarity < -0.3:
-        result_str += "\nFairly negative! Average Polarity (-1 - negative, +1 - positive): " + str(avg_polarity)
-
-    if 0.3 < avg_polarity <= 1.0:
-        result_str += "\nFairly positive! Average Polarity (-1 - negative, +1 - positive): " + str(avg_polarity)
-
+    for t in host_name_filtered_tweets:
+        if t and top_emotion in t[1]["text"]:
+            result_str +="For example \n"
+            result_str += t[1]["text"]
+            break
     return result_str
 
 
